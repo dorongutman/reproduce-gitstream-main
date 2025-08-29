089e9308c6f70f4a3baef7a91ca631f2e1ca0ecb
Doron Gutman <doron@honeybook.com>
2025-08-29 23:00:51 +0300

    debug

--- .cm/plugins/filters/checkStatusEquals/index.js
+++ .cm/plugins/filters/checkStatusEquals/index.js
@@ -14,60 +14,60 @@
 
 const DEBUG = true;
 
-const checkStatusEquals = async (commitShaOrBranch, checkName, expectedState, githubOrg, githubRepo, token, callback) => {
+const checkStatusEquals = (commitShaOrBranch, checkName, expectedState, githubOrg, githubRepo, token) => { //, callback) => {
     return false;
-    return callback(null, false);
-    try {
-        // GitHub API endpoint for check runs
-        const apiUrl = `https://api.github.com/repos/${githubOrg}/${githubRepo}/statuses/${commitShaOrBranch}`;
+    // return callback(null, false);
+    // try {
+    //     // GitHub API endpoint for check runs
+    //     const apiUrl = `https://api.github.com/repos/${githubOrg}/${githubRepo}/statuses/${commitShaOrBranch}`;
         
-        // Prepare headers with authentication
-        const headers = {
-            'Accept': 'application/vnd.github.v3+json',
-            'User-Agent': 'gitStream-plugin'
-        };
+    //     // Prepare headers with authentication
+    //     const headers = {
+    //         'Accept': 'application/vnd.github.v3+json',
+    //         'User-Agent': 'gitStream-plugin'
+    //     };
         
-        if (DEBUG) {
-            console.log('GitHub API URL:', apiUrl);
-        }
+    //     if (DEBUG) {
+    //         console.log('GitHub API URL:', apiUrl);
+    //     }
 
-        // Add authorization header if token is provided
-        if (token) {
-            headers['Authorization'] = `Bearer ${token}`;
-        }
+    //     // Add authorization header if token is provided
+    //     if (token) {
+    //         headers['Authorization'] = `Bearer ${token}`;
+    //     }
         
-        // Make the API request
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: headers
-        });
+    //     // Make the API request
+    //     const response = await fetch(apiUrl, {
+    //         method: 'GET',
+    //         headers: headers
+    //     });
 
-        if (!response.ok) {
-            if (DEBUG) {
-                console.log('GitHub API request failed:', response);
-            }
-            throw new Error(`GitHub API request failed: ${response.status} ${response.statusText}`);
-        }
+    //     if (!response.ok) {
+    //         if (DEBUG) {
+    //             console.log('GitHub API request failed:', response);
+    //         }
+    //         throw new Error(`GitHub API request failed: ${response.status} ${response.statusText}`);
+    //     }
 
-        const data = await response.json();
+    //     const data = await response.json();
 
-        if (DEBUG) {
-            console.log('GitHub API response:', data);
-        }
+    //     if (DEBUG) {
+    //         console.log('GitHub API response:', data);
+    //     }
         
-        // Find the check run with the specified name
-        const checkRun = data.find(check => check.context === checkName);
+    //     // Find the check run with the specified name
+    //     const checkRun = data.find(check => check.context === checkName);
         
-        if (!checkRun) {
-            throw new Error('Check not found');
-        }
+    //     if (!checkRun) {
+    //         throw new Error('Check not found');
+    //     }
 
-        // Return the status of the check run
-        return callback(null, checkRun.state === expectedState);
+    //     // Return the status of the check run
+    //     return callback(null, checkRun.state === expectedState);
         
-    } catch (exception) {
-        return callback('Error checking status: ' + exception, false);
-    }
+    // } catch (exception) {
+    //     return callback('Error checking status: ' + exception, false);
+    // }
 };
 
 module.exports = {
